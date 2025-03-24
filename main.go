86051fe565a8f2f13003e8f3eb2f3ed95d221717
りおちん <118029.ichikama@gmail.com>
2025-03-24 22:19:27 +0900

    Hello main page

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import(
+	"fmt"
+	"net/http"
+	"html/template"
+	// "log"
+)
+
+type Memo struct {
+	Title string
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		memos := []Memo{
+			{"Goでwebアプリを作った"},
+			{"Markdown対応のwebアプリ"},
+		}
+
+		tmpl, err := template.ParseFiles("templates/index.html")
+		if err != nil {
+			http.Error(w, "テンプレートの読み込みに失敗しました", http.StatusInternalServerError)
+			return
+		}
+
+		err = tmpl.Execute(w, memos)
+		if err != nil {
+			http.Error(w, "テンプレート実行エラー", http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
